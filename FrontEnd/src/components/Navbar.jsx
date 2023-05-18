8ce0cd2528ab721e6dbe4aa7e4de38624bc1c492
Junior Rojas <https://github.com/hanzlyr1>
2023-05-17 22:56:18 -0500

    modifying the navigation bar and creating the record

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/navbar.css';
 
 const Navbar = () => {
+
+  const [bars, setBars] = useState(false)
+  const [dep, setDep] = useState(false)
+    
   return (
     <nav className='navbar'>
 
             <div className='navbar_logo'>
-                <h1>Logo</h1>
+                <div className='logo logo1'>logo</div>
             </div>
-            <ul className='navbar_link'>
-            <Link to="/">Inicio</Link>
-            <Link to="/logeed">Logeed</Link>
-            <Link to="/cards">Tarjetas</Link>
-            <Link to="/profile">Perfil</Link>
-            <Link to="/actividad">Historial Crediticio</Link>
-            <Link to="/send-money">Enviar Dinero</Link>
+            <button className="botton-bars" onClick={() => {setBars(!bars); setDep(!dep);}}><i className="fa-solid fa-bars"></i></button>
+            <span className={`navbar_link  ${bars ? "ocultar": ""} ${dep ? "dep" : ""} `}>
+            <Link className='nav-link' to="/">Inicio</Link>
+            <Link className='nav-link' to="/logIn">LogIn</Link>
+            <Link className='nav-link' to="/logeed">Logeed</Link>
+            <Link className='nav-link' to="/cards">Tarjetas</Link>
+            <Link className='nav-link' to="/profile">Perfil</Link>
+            <Link className='nav-link' to="/actividad">Historial</Link>
+            <Link className='nav-link' to="/send-money">Enviar Dinero</Link>
 
-            </ul>
-
+            </span>
+            
         </nav>
   )
 }
